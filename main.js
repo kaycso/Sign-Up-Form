fd4dc9bfd146028f4816b8dba2d2ac398b5e848e
Kayc Santos Oliveira <kaycsoliveira@hotmail.com>
2023-06-15 13:40:27 -0300

    bug fix

--- main.js
+++ main.js
@@ -1,54 +1,83 @@
 const buttonRegisterUser = document.getElementById('registerUser')
+const errorEmailMessage = document.getElementById('error-email')
+const errorPasswordMessage = document.getElementById('error-password')
+const errorConfirmedPassword = document.getElementById('error-passwordConfirmed')
 let passwordElement = document.getElementById('password')
 
-buttonRegisterUser.addEventListener('click', registerVerification)
+buttonRegisterUser.disabled = true
 
-document.getElementById('kickNumber').onkeypress = function(e) {
+// código para permitir apenas letras no input do nome
+document.getElementById('name').onkeypress = function(e) {
     var chr = String.fromCharCode(e.which);
-    if("1234567890".indexOf(chr) < 0)
+    if("qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM".indexOf(chr) < 0)
     return false;
 }
 
-function registerVerification() {
+document.getElementById("confirmedEmail").addEventListener('input', function(event){
     emailVerification();
-    passwordValidation();
-    passwordVerification();
-}
+})
+
+document.getElementById('password').addEventListener('input', function(event){
+    passwordValidation()
+})
+
+document.getElementById('confirmedPassword').addEventListener('input', function(event){
+    passwordVerification()
+})
 
 function emailVerification() {
     let email = document.getElementById('email').value;
     let confirmedEmail = document.getElementById('confirmedEmail').value;
-    const errorEmailMessage = document.getElementById('error-email');
 
-    if (email !== confirmedEmail & confirmedEmail !== '') {
+    if (email !== confirmedEmail) {
         errorEmailMessage.classList.add('displayOn');
-        return
+        buttonRegisterUser.disabled = true
+
+        return 
     }
     
     errorMessageExist(errorEmailMessage)
+    inputVerification()
 }
 
 
 function passwordValidation() {
-    const errorPasswordMessage = document.getElementById('error-password')
-    if(password.value.length < 8 && password.value !== '') {
+    if(passwordElement.value.length < 8) {
         errorPasswordMessage.classList.add('displayOn')
+        buttonRegisterUser.disabled = true
         return
     }
 
     errorMessageExist(errorPasswordMessage)
+    inputVerification()
 }
 
 function passwordVerification() {
     let confirmedPassword = document.getElementById('confirmedPassword').value;
-    const errorConfirmedPassword = document.getElementById('error-passwordConfirmed');
 
-    if (password.value !== confirmedPassword && confirmedPassword !== "") {
+    if (password.value !== confirmedPassword) {
         errorConfirmedPassword.classList.add('displayOn')
+        buttonRegisterUser.disabled = true
         return
     }
 
     errorMessageExist(errorConfirmedPassword)
+    inputVerification()
+}
+
+function inputVerification() {
+    if(errorEmailMessage.classList.value == 'displayOn') {
+        buttonRegisterUser.disabled = true
+        return
+    } else if(errorPasswordMessage.classList.value == 'displayOn'){
+        buttonRegisterUser.disabled = true
+        return
+    } else if(errorConfirmedPassword.classList.value == 'displayOn'){
+        buttonRegisterUser.disabled = true
+        return
+    } else {
+        return buttonRegisterUser.disabled = false
+    }
 }
 
 function errorMessageExist(message) {
